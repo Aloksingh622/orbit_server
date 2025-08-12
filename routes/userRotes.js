c9b124b4ae1cc0d78bfa8cb27ce16904740a42ff
Alok Singh <saloksingh320@gmail.com>
2025-08-12 21:34:31 +0530

    descover2

--- routes/userRotes.js
+++ routes/userRotes.js
@@ -9,7 +9,7 @@ const userRouter = express.Router();
 userRouter.get('/data', protect, getUserData)
 userRouter.post('/update', upload.fields([{name: 'profile', maxCount: 1}, {name: 'cover', maxCount: 1}]), protect, updateUserData)
 userRouter.post('/discover', protect, discoverUsers)
-userRouter.post('/random', protect, getRandomUnconnectedUsers)
+userRouter.get('/random', protect, getRandomUnconnectedUsers)
 userRouter.post('/follow', protect, followUser)
 userRouter.post('/unfollow', protect, unfollowUser)
 userRouter.post('/connect', protect, sendConnectionRequest)
